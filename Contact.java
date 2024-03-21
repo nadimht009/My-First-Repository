32fc3f57747a36d39dac3ee46b78b8820670dc41
nadimht009 <126472105+nadimht009@users.noreply.github.com>
2024-03-22 00:01:26 +0600

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,16 @@
+public class Contact {
+    String name;
+    String number;
+    String email;
+
+    Contact()
+    {
+
+    }
+    Contact(String name, String number, String email)
+    {
+        this.email = email;
+        this.name = name;
+        this.number = number;
+    }
+}
